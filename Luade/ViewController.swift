97296055562fca49d5886c5ea8515164b2a7cfb4
Adrian Labbé <adrilabbelol@gmail.com>
2018-11-25 16:01:44 -0300

    Initial Commit

---
+++ Luade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Luade
+//
+//  Created by Adrian Labbe on 11/25/18.
+//  Copyright © 2018 Adrian Labbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
