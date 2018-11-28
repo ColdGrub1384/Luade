e8a7eaac42997123a877054043126c3a5143cdcd
Adrian Labbé <adrilabbelol@gmail.com>
2018-11-28 17:49:21 -0300

    Add support for Siri Shortcuts

--- Luade/Singletons/AppDelegate.swift
+++ Luade/Singletons/AppDelegate.swift
@@ -34,5 +34,38 @@ import ios_system
     func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
         return true
     }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        
+        let docBrowser = DocumentBrowserViewController.visible
+        let root = application.keyWindow?.rootViewController
+        
+        func runScript() {
+            if let path = userActivity.userInfo?["filePath"] as? String {
+                
+                let url = URL(fileURLWithPath: RelativePathForScript(URL(fileURLWithPath: path)), relativeTo: FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first)
+                
+                if FileManager.default.fileExists(atPath: url.path) {
+                    docBrowser?.openDocument(url, run: true)
+                } else {
+                    let alert = UIAlertController(title: Localizable.Errors.errorReadingFile, message: nil, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: Localizable.ok, style: .cancel, handler: nil))
+                    root?.present(alert, animated: true, completion: nil)
+                }
+            } else {
+                print("Invalid shortcut!")
+            }
+        }
+        
+        if root?.presentedViewController != nil {
+            application.keyWindow?.rootViewController?.dismiss(animated: true, completion: {
+                runScript()
+            })
+        } else {
+            runScript()
+        }
+        
+        return true
+    }
 }
 
