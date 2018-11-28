f1a968b5c84e7a64e5585cc5bed6e8eb56315717
Adrian Labbé <adrilabbelol@gmail.com>
2018-11-28 17:37:12 -0300

    Ignore pods

--- Pods/Target Support Files/InputAssistant/InputAssistant-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_InputAssistant : NSObject
-@end
-@implementation PodsDummy_InputAssistant
-@end
--- Pods/Target Support Files/Pods-Luade/Pods-Luade-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Luade : NSObject
-@end
-@implementation PodsDummy_Pods_Luade
-@end
