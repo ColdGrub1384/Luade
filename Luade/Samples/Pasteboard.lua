e27f9e1216b9a09ef14ee3048649e91e43481b71
Adrian Labbé <adrilabbelol@gmail.com>
2018-12-08 18:03:48 -0300

    Update sample

--- Luade/Samples/Pasteboard.lua
+++ Luade/Samples/Pasteboard.lua
@@ -1,4 +1,4 @@
 -- This example shows how copy and pasting text.
 
 print("Copied text: "..pasteboard.string()) -- Paste text
-pasteboard.setString("Hello World!") -- Copy "Hello World!"
+pasteboard.setString("Hello World!") -- Copy 'Hello World!'
